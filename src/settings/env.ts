9b01b2ff16d36b00957738fb8da964ee4af64e85
Eric Freitas <freitaseric@proton.me>
2024-10-20 14:29:57 -0400

    chore: initial commit

---
+++ src/settings/env.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+    BOT_TOKEN: z.string({ description: "Discord Bot Token is required" }).min(1),
+    WEBHOOK_LOGS_URL: z.string().url().optional(),
+    SERVER_PORT: z.number({ coerce: true }).optional(),
+    SERVER_BASE_URL: z.string().optional(),
+    // Env vars...
+});
+
+type EnvSchema = z.infer<typeof envSchema>;
+
+export { envSchema, type EnvSchema };
